7b161da914320fcfe30c58ca6e4d12a68d7557b8
Jeongmmin <seo88kr@hanmail.net>
2022-04-19 18:25:24 +0900

    ✍ Study Reordering 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from './atom';
 
 const Wrapper = styled.div`
   display: flex;
@@ -31,10 +33,14 @@ const Card = styled.div`
   margin-bottom: 5px;
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
-  const onDragEnd = () => {};
+
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  // Drag가 끝났을 때  동작하는 함수
+  const onDragEnd = ({destination, source}:DropResult) => {
+    destination.
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -44,7 +50,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
+                  <Draggable key={index} draggableId={toDo} index={index}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
@@ -69,7 +75,23 @@ function App() {
 export default App;
 
 /**
-  draggable 을 array로 만들기
+  drop 했을 때 순서 변하도록 설정해볼 것이다.
 
-  {magic.placeholder} : 요소가 하나 drag 될 때 board 사이즈가 변하지 않도록 설정해줌
+  console로 관찰가능
+  destination:
+droppableId: "one"
+index: 0
+
+source:
+droppableId: "one"
+index: 5
+
+해야하는 것? -> source.index를 지운다. destination index로 다시 arrqy에 넣는다.
+
+const x = ["a", "b", "c", "d", "e", "f"];
+undefined
+x.splice(0, 1)
+['a']
+x.splice(2, 0, "a")
+[]
 */
